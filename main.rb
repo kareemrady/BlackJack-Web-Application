4d36448391eb468d93dc515fa07b65c620005570
Kareem Rady <kareem_rady@yahoo.com>
2013-06-18 21:31:44 -0400

    Updated Online BlackJack Game

--- main.rb
+++ main.rb
@@ -20,6 +20,8 @@ post '/game' do
 	session[:new_player] = params[:new_player]
 	session[:dealer_cards] = []
 	session[:player_cards]= []
+	session[:dealer_score]= 0
+	session[:player_score]= 0
 	suit = ["H", "C", "S", "D"]
 	value = ["A", "2", "3", "4", "5","6", "7", "8", "9", "10", "J", "K", "Q"]
 	session[:deck] = suit.product(value).shuffle!
@@ -27,6 +29,21 @@ post '/game' do
 	session[:dealer_cards] << session[:deck].pop
 	session[:player_cards] << session[:deck].pop
 	session[:player_cards] << session[:deck].pop
+	session[:player_cards].each do |s, v|
+		if v == "A"
+			session[:player_score] += 11 
+		else
+		v.to_i == 0 ? session[:player_score] +=10 : session[:player_score] += v.to_i
+	end
+end
+	session[:dealer_cards].each do |s, v|
+		if v == "A" 
+			session[:dealer_score] += 11 
+		else
+		v.to_i == 0 ? session[:dealer_score] +=10 : session[:dealer_score] += v.to_i
+	end
+end
+
 	erb :game, :layout => false
 
 end
@@ -34,6 +51,8 @@ end
 post '/hit' do
 session[:player_cards] << session[:deck].pop
 session[:dealer_cards] << session[:deck].pop
+
+
 erb :hit, :layout => false
 end
 
